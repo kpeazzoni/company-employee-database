679a141bd5ac183bc82fcf5ee89714e3ba2bc2d0
Karen Peazzoni <119969744+kpeazzoni@users.noreply.github.com>
2023-02-03 20:57:47 -0500

    fixed all the buggy bugs

--- db/seeds.sql
+++ db/seeds.sql
@@ -9,17 +9,17 @@ VALUES ("Receptionist"),
        ("World's Best Boss");
 
 INSERT INTO role (title, salary, department_id)
-VALUES ('Lead Receptionist', 40000, 01),
-       ('Sales Lead', 80000, 02),
-       ('QA Rep', 55000, 03),
-       ('Accountant', 90000, 04),
-       ('HR Rep', 65000, 05),
-       ('Boss AKA Manager', 95000, 06);
+VALUES ('Lead Receptionist', 40000, 1),
+       ('Sales Lead', 80000, 2),
+       ('QA Rep', 55000, 3),
+       ('Accountant', 90000, 4),
+       ('HR Rep', 65000, 5),
+       ('Boss AKA Manager', 95000, 6);
 
-INSERT INTO employee (first_name, last_name, role_id)
-VALUES ('Pam','Beasley', 01),
-       ('Dwight', 'Schrute', 02),
-       ('Greed', 'Bratton', 03),
-       ('Oscar', 'Martinez', 04),
-       ('Toby', 'Flenderson', 05),
-       ('Michael', 'Scott', 06);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Michael', 'Scott', 6, NULL),
+       ('Pam','Beasley', 1, 1),
+       ('Dwight', 'Schrute', 2, 1),
+       ('Greed', 'Bratton', 3, 1),
+       ('Oscar', 'Martinez', 4, 1),
+       ('Toby', 'Flenderson', 5, 1);
--- index.js
+++ index.js
@@ -1,14 +1,9 @@
 
-const { default: inquirer } = require("inquirer");
+const inquirer = require("inquirer");
 const mysql = require('mysql2');
+const express = require('express');
+require('console.table');
 const PORT = process.env.PORT || 3001;
-
-
-const utils = require("util");
-db.query = utils.promisify(db.query);
-// Express middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
 
 // Connect to database
 const db = mysql.createConnection(
@@ -18,34 +13,63 @@ const db = mysql.createConnection(
     user: 'root',
     // MySQL password
     password: 'password',
-    database: 'classlist_db'
+    database: 'business_db'
   },
-  console.log(`Connected to the classlist_db database.`)
+  console.log(`Connected to the business_db database.`)
 );
 
+const utils = require("util");
+db.query = utils.promisify(db.query);
+
 // delcaring globally scoped variables for db.queries
-const roles = db.query`SELECT role_id, role_title, role_salary FROM roles;`
-const employees = db.query`SELECT employee_id, first_name, last_name, role_id, department_id, salaries, managers FROM employee`
-const departments = db.query`SELECT department_name, department_id FROM department`
+
+async function viewRoles() {
+  const roles = await db.query(`SELECT id, title, salary FROM role;`)
+  console.table(roles)
+  loadMainQuestions();
+}
+// line 38- joining employee.role_id (employee table) to ROLE TABLE ID something.something = table name.column name
+async function viewEmployees() {
+  const employees = await db.query(`SELECT employee.id, employee.first_name AS "first name", employee.last_name 
+  AS "last name", role.title, departments.name AS department, role.salary, 
+  concat(manager.first_name, " ", manager.last_name) AS manager
+  FROM employee
+  LEFT JOIN role
+  ON employee.role_id = role.id 
+  LEFT JOIN departments
+  ON role.department_id = departments.id
+  LEFT JOIN employee manager
+  ON manager.id = employee.manager_id`)
+  console.table(employees);
+  loadMainQuestions();
+}
+
+async function viewDepartments() {
+  const departments = await db.query(`SELECT name, id FROM departments`)
+  console.table(departments);
+  loadMainQuestions();
+}
 
 
+loadMainQuestions();
 
 function loadMainQuestions() {
-  inquirer.prompt[{
+  inquirer.prompt([{
     type: 'list',
     name: 'action',
     message: 'what would you like to do?',
-    choices: ['View All departments', 'View all roles', 'View all employees', 'Add department', 'Add a role', 'Add an employee', 'Update an employee role', 'Quit']
-  }].then((res) => {
+    choices: ['View all departments', 'View all roles', 'View all employees', 'Add department', 'Add a role', 'Add an employee', 'Update an employee role', 'Quit']
+  }]).then((res) => {
     switch (res.action) {
       case 'View all departments':
-        departments;
+        viewDepartments();
         break;
       case 'View all roles':
-        roles;
+        viewRoles();
         break;
       case 'View all employees':
-        employees;
+        viewEmployees();
+        
         break;
       case 'Add department':
         addDepartment();
@@ -61,25 +85,31 @@ function loadMainQuestions() {
         break;
       default:
         console.log('All Done!')
+        process.exit();
     }
   })
 };
 
 async function addDepartment() {
 
-  inquirer.prompt([{
+  const answer = await inquirer.prompt([{
     type: 'input',
     name: 'newDepartment',
     message: 'What is the name of the new department?'
   }]);
-  const newDept = await db.query(`INSERT INTO departments (name) VALUES ${answer.newDepartment}`)
-  console.table(newDept);
+  const newDept = await db.query(`INSERT INTO departments (name) VALUES ("${answer.newDepartment}")`)
+  console.log(`Successfully added the ${answer.newDepartment} into departments`);
   loadMainQuestions();
 };
 
 async function addRole() {
+  const roleResult = await db.query(`SELECT * FROM role`)
+  let finalRoles = roleResult.map(({ id, title }) => ({
+    value: id,
+    name: `${title}`,
+  }));
 
-  inquirer.prompt([
+  const answer = await inquirer.prompt([
     {
       type: 'input',
       name: 'newRole',
@@ -93,16 +123,26 @@ async function addRole() {
     {
       type: 'list',
       name: 'department',
-      choices: departments
+      choices: finalRoles
     },
   ]);
-  const newRole = await db.query(`INSERT INTO roles (title) VALUES (${answer.newRole} ${answer.salary} ${answer.department})`)
+
+  const newRole = await db.query(`INSERT INTO role (title, salary, department_id) VALUES ("${answer.newRole}", "${answer.salary}", "${answer.department}")`)
   console.table(newRole);
   loadMainQuestions();
 };
 async function addEmployee() {
-
-  inquirer.prompt([
+  const roleResult = await db.query(`SELECT * FROM role`)
+  let finalRoles = roleResult.map(({ id, title }) => ({
+    value: id,
+    name: `${title}`,
+  }));
+  const employeeResult = await db.query('SELECT * FROM employee');
+  const updateEmployee = employeeResult.map(({ id, first_name, last_name }) => ({
+    value: id,
+    name: `${first_name} ${last_name}`,
+  }))
+  const answer = await inquirer.prompt([
     {
       type: 'input',
       name: 'newFirst',
@@ -117,54 +157,57 @@ async function addEmployee() {
       type: 'list',
       name: 'newRole',
       message: 'What is the role you would like to add?',
-      choices: roles
+      choices: finalRoles
     },
     {
       type: 'list',
       name: 'newMan',
       message: 'Who is the manager of the new employee?',
-      choices: employees
+      choices: updateEmployee
     },
   ]);
-  const newEmployee = await db.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (${answer.newFirst} ${answer.newLast} ${answer.newRole}${answer.newMan}`)
+  const newEmployee = await db.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("${answer.newFirst}", "${answer.newLast}", "${answer.newRole}", "${answer.newMan}")`)
   console.table(newEmployee);
   loadMainQuestions();
-}
+
+};
 
 async function updateEmployee() {
-  const sql = `SELECT first_name, last_name, id FROM employee`;
-  const employeeResult = await db.query(sql);
-  const updateEmployee = employeeResult.map(({ id, first_name, last_name }) => ({
-    value: id,
-    name: `${first_name} ${last_name}`,
-  }))
-  const roleResult = await db.query(`SELECT * FROM roles`)
-  let finalRoles = roleResult.map(({ id, title }) => ({
-    value: id,
-    name: `${title}`,
-  }))
-  const answer = inquirer.prompt(
-    {
-      type: 'list',
-      name: 'employee_id',
-      message: 'Which employee employee do you want to update?',
-      choices: employees,
-    },
-    {
-      type: 'list',
-      name: 'role',
-      message: 'updated role',
-      choices: roles,
-    })
-  let updateSQL = `UPDATE employee SET role_id = ${answer.role} WHERE id = ${anwer.employee.id}`
-  console.table(updateSQL, updateEmployee, finalRoles);
-
-  await db.query(updateSQL)
-  console.log('updated....')
   try {
+    const employeeResult = await db.query(`SELECT first_name, last_name, id FROM employee`)
+    const roleResult = await db.query('select title, id from role')
+
+    const employees = employeeResult.map(({ id, first_name, last_name }) => ({
+      value: id,
+      name: `${first_name} ${last_name}`,
+    }));
+
+    const roles = roleResult.map(({ id, title }) => ({
+      value: id,
+      name: `${title}`,
+    }));
+
+    const answers = await inquirer.prompt([
+      {
+        type: 'list',
+        name: 'employee_id',
+        message: "Which employee's role do you want to update?",
+        choices: employees,
+      },
+      {
+        type: 'list',
+        name: 'role_id',
+        message: 'Updated role',
+        choices: roles,
+      },
+    ])
+    let updateSQL = `UPDATE employee SET role_id = "${answers.role_id}" 
+    WHERE id = "${answers.employee_id}"`
+
+    await db.query(updateSQL)
+    console.log('Employee role is successfully updated....')
+    loadMainQuestions()
   } catch (error) {
     console.log(error)
   }
-  loadMainQuestions();
 };
-
